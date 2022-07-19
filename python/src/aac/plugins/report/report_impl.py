456b9698e74be12a0fddde55d3d994bbc01b685f
jondavid-black <black.jondavid@gmail.com>
2022-07-19 20:16:30 +0000

    initial simple report working

--- python/src/aac/plugins/report/report_impl.py
+++ python/src/aac/plugins/report/report_impl.py
@@ -6,16 +6,18 @@ from aac.plugins.plugin_execution import PluginExecutionResult, plugin_result
 from aac.validate import validated_source
 from aac.lang.definition_helpers import get_definitions_by_root_key
 from aac.lang.definitions.definition import Definition
-from aac.lang.definition_helpers import convert_parsed_definitions_to_dict_definition, get_definitions_by_source_uri
+from aac.lang.definition_helpers import get_definitions_by_source_uri
+from aac.lang.references import get_reference_target_value
 
 import yaml
 import csv
 import io
+from typing import Optional
 
 plugin_name = "report"
 
 
-def report(report_definition_file: str, architecture_file: str, output_file: str) -> PluginExecutionResult:
+def report(report_definition_file: str, architecture_file: str, output_file: Optional[str] = None) -> PluginExecutionResult:
     """
     Generate YAML output from an Arch-as-Code report definition.
 
@@ -34,14 +36,16 @@ def report(report_definition_file: str, architecture_file: str, output_file: str
             ret_val += yaml_content
 
         # TODO if output_file is provided, write ret_val to file.  Should we also provide content for cli output?
-        print(f"YAML = {yaml_content}")
+        if output_file is not None:
+            print(f"print YAML to {output_file}")
+        print(f"YAML = {yaml_content}")  # TODO delete this line
         return yaml_content
 
     with plugin_result(plugin_name, _gen_yaml_report) as result:
         return result
 
 
-def report_csv(report_definition_file: str, architecture_file: str, output_file: str) -> PluginExecutionResult:
+def report_csv(report_definition_file: str, architecture_file: str, output_file: Optional[str] = None) -> PluginExecutionResult:
     """
     Generate CSV output from an Arch-as-Code report definition.
 
@@ -56,14 +60,14 @@ def report_csv(report_definition_file: str, architecture_file: str, output_file:
         for report in report_content_list:
             # convert report content to csv
             field_names = []
-            for cont in report["content"]:
+            for cont in report["data"]:
                 for fn in cont:
                     field_names.append(fn)
             header_names = set(field_names)
             output = io.StringIO()
             writer = csv.DictWriter(output, fieldnames=header_names, delimiter=',')
             writer.writeheader()
-            writer.writerows(report["content"])
+            writer.writerows(report["data"])
 
             csv_report = output.getvalue()
 
@@ -71,8 +75,10 @@ def report_csv(report_definition_file: str, architecture_file: str, output_file:
                 ret_val += "\n\n"
             ret_val += csv_report
 
-            # TODO if output_file is provided, write ret_val to file.  Should we also provide content for cli output?
-
+        # TODO if output_file is provided, write ret_val to file.  Should we also provide content for cli output?
+        if output_file is not None:
+            print(f"print YAML to {output_file}")
+        print(f"CSV = {ret_val}")  # TODO remove this line
         return ret_val
 
     with plugin_result(plugin_name, _gen_csv_report) as result:
@@ -88,75 +94,37 @@ def _generate_report(report_file: str, architecture_file: str) -> dict:
 
     report_defs = []
     report_results = []
-    with validated_source(report_file) as report_result:
+    with validated_source(report_file) as report_result, validated_source(architecture_file) as arch_result:
         # get reports defined in the report file
         report_defs = get_definitions_by_root_key("report", get_definitions_by_source_uri(report_file, report_result.definitions))
 
         print(f"report_defs = {[entry.name for entry in report_defs]}")
 
-        for report_def in report_defs:  # TODO fix magic string
+        for report_def in report_defs:
             report_result = {}
-            fields = report_def.get_fields()
+            report_fields = report_def.get_top_level_fields()
 
             # get report result metadata
-            report_result["title"] = fields["name"]  # TODO fix magic string
-            report_result["description"] = fields["description"]  # TODO fix magic string
+            report_result["title"] = report_fields["name"]
+            report_result["description"] = report_fields["description"]
 
             # get data content
             content = []
+            for arch_definition in arch_result.definitions:
+                data_result = {}
+                for data_item in report_fields["data"]:
+                    data_item_value = get_reference_target_value(data_item["source"], arch_definition)
+                    if len(data_item_value) == 0:
+                        data_result[data_item["name"]] = ""
+                    elif len(data_item_value) == 1:
+                        data_result[data_item["name"]] = data_item_value[0]
+                    else:
+                        data_result[data_item["name"]] = data_item_value
 
-            for get_me in fields["data"]:
-                column_name = get_me["name"]
-                column_desc = get_me["description"]
-                column_value = get_me["source"]  # TODO fix magic string)
+                content.append(data_result)
 
-            report_result["content"] = content
+            report_result["data"] = content
 
             report_results.append(report_result)
 
-    with validated_source(architecture_file) as validation_result:
-        definitions_as_dictionary = convert_parsed_definitions_to_dict_definition(validation_result.definitions)
-
-        for report_def in get_definitions_by_root_key("report", validation_result.definitions):  # TODO fix magic string
-
-            report_result = {}
-            fields = report_def.get_fields()
-
-            # get report result metadata
-            report_result["title"] = fields["name"]  # TODO fix magic string
-            report_result["description"] = fields["description"]  # TODO fix magic string
-            # get data types to extract report content from
-            source_types = []
-            things_to_get = fields["retrieve"]  # TODO fix magic string
-            for get_me in things_to_get:
-                found_type = get_me["source_type"]  # TODO fix magic string
-                source_types.append(found_type)
-            source_types = set(source_types)
-
-            # assemble content data
-            content_source = []  # list of dicts for the source types
-            if len(source_types) == 1:
-                # this is the simple case we'll start with
-                root_key = list(source_types)[0]
-                # get parsed dicts that match the root key
-                for name in definitions_as_dictionary:
-                    if root_key in definitions_as_dictionary[name]:
-                        content_source.append(definitions_as_dictionary[name])
-            else:
-                # don't have this completely worked out yet
-                raise NotImplementedError("complex reports not implemented yet")
-
-            # populate report content
-            content = []
-            for content_source_item in content_source:
-                content_item = {}
-                for get_item in things_to_get:
-                    report_field_name = get_item["name"]
-                    report_field_value = content_source_item[get_item["source_type"]][get_item["source_field_name"]]
-
-                    content_item[report_field_name] = report_field_value
-                content.append(content_item)
-            report_result["content"] = content
-
-            reports.append(report_result)
-    return reports
+    return report_results
--- python/tests/plugins/test_report_impl.py
+++ python/tests/plugins/test_report_impl.py
@@ -1,27 +1,144 @@
 from unittest import TestCase
 
+from tempfile import TemporaryDirectory
+from tests.helpers.io import temporary_test_file
+from tests.helpers.plugins import YAML_FILE_EXTENSION
+
 from aac.plugins.plugin_execution import PluginExecutionStatusCode
 from aac.plugins.report.report_impl import report, report_csv
 
+TEST_SIMPLE_REPORT_YAML = f"""
+report:
+  name: Parts List
+  description: A list of all parts for the apartment.
+  data:
+    - name: Part Make
+      description: The vendor of the part.
+      source: part.make
+    - name: Part Model
+      description: The manufacturer model identifier.
+      source: part.model
+    - name: Part Unit Cost
+      description: What the part costs.
+      source: part.unit_cost
+    - name: Part Description
+      description: The manufacturer description of the part.
+      source: part.description
+"""
+
+TEST_SIMPLE_ARCH_PART_YAML = f"""
+part:
+  name: Coasters
+  make: HUAOAO
+  model: Cork Coaster Set
+  description: Set of 12 plain cork coasters.
+  unit_cost: 7.19
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com/Coaster-Absorbent-Resistant-Reusable-Coasters/dp/B08PZ15J2N
+---
+part:
+  name: TV
+  make: Samsung
+  model: S-Z452-OG2331-55-HD
+  description: 55 inch high-definition smart tv
+  unit_cost: 682.23
+  quote_type: Web_Reference
+  quote: https://www.google.com
+---
+part:
+  name: Wifi_Router
+  make: NetGear
+  model: N334521939
+  description: 802.11 a/b/g
+  unit_cost: 88.33
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: Sound_Bar
+  make: Loudness Inc
+  model: LI-35-ST-HDMI-BT-334
+  description: Something better than the built-in tv speakers
+  unit_cost: 120.23
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: X_Box
+  make: Microsoft
+  model: X-Box One v2
+  description: Streaming and gaming device
+  unit_cost: 599.99
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: Coffee_Pot
+  make: Perky Mornings
+  model: Dual-brew 2200
+  description: Coffee maker with drip and k-cup
+  unit_cost: 199.99
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: Blender
+  make: Grind House
+  model: Liquificationinator 1000
+  description: 7 setting industrial strength blender with pulse
+  unit_cost: 99.99
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: Pans
+  make: Affordable Living
+  model: 12-al-33288
+  description: 12 piece aluminum pots and pans
+  unit_cost: 299.99
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: Dishes
+  make: Affordable Living
+  model: 2251-AHRSU-8
+  description: 8 place setting dinner ware set
+  unit_cost: 129.99
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: Utensils
+  make: Affordable Living
+  model: XG-33445
+  description: Complete set of knives, spatulas, measuring cups, and whisks
+  unit_cost: 99.99
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+---
+part:
+  name: Silverware
+  make: Affordable Living
+  model: 8835-AHTTR-12
+  description: 12 settings of fork, spoon, and butter knife
+  unit_cost: 139.99
+  quote_type: Web_Reference
+  quote: https://smile.amazon.com
+"""
 
 class TestReport(TestCase):
-    def test_report(self):
-        # TODO: Write tests for report
+    def test_simple_report(self):
+        with TemporaryDirectory() as temp_directory, temporary_test_file(TEST_SIMPLE_REPORT_YAML, dir=temp_directory, suffix=YAML_FILE_EXTENSION) as report_yaml, temporary_test_file(TEST_SIMPLE_ARCH_PART_YAML, dir=temp_directory, suffix=YAML_FILE_EXTENSION) as arch_yaml:
 
-        report_definition_file = str()
-        architecture_file = str()
-        --output_file = str()
-        
-        result = report(report_definition_file=report_definition_file, architecture_file=architecture_file, --output_file=--output_file)
+            result = report(report_yaml.name, arch_yaml.name)
+            print(f"messages = {result.messages}")
         self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
-    
-    def test_report_csv(self):
+
+    def test_simple_report_csv(self):
         # TODO: Write tests for report_csv
+        with TemporaryDirectory() as temp_directory, temporary_test_file(TEST_SIMPLE_REPORT_YAML, dir=temp_directory, suffix=YAML_FILE_EXTENSION) as report_yaml, temporary_test_file(TEST_SIMPLE_ARCH_PART_YAML, dir=temp_directory, suffix=YAML_FILE_EXTENSION) as arch_yaml:
 
-        report_definition_file = str()
-        architecture_file = str()
-        --output_file = str()
-        
-        result = report_csv(report_definition_file=report_definition_file, architecture_file=architecture_file, --output_file=--output_file)
+            result = report_csv(report_yaml.name, arch_yaml.name)
+            print(f"messages = {result.messages}")
         self.assertEqual(result.status_code, PluginExecutionStatusCode.SUCCESS)
-    
